c509191937f6d0f79a1b1e7b31363596f1f28ca1
Naoto Onishi <onikiri@Naotos-MacBook-Pro.local>
2017-03-08 13:07:13 +1100

    need to add framework file to build

---
+++ Libraries/ICGVideoTrimmer/iOS/Binding/ICGVideoTrimmer.framework/Headers/ICGRulerView.h
@@ -0,0 +1,28 @@
+//
+//  ICGRulerView.h
+//  ICGVideoTrimmer
+//
+//  Created by Huong Do on 1/25/15.
+//  Copyright (c) 2015 ichigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+static const CGFloat RulerViewLeftMargin = 10.0f;
+
+@interface ICGRulerView : UIView
+
+@property (assign, nonatomic) CGFloat widthPerSecond;
+@property (strong, nonatomic) UIColor *themeColor;
+
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+
+- (nullable instancetype)initWithCoder:(NSCoder *)aDecoder NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)initWithFrame:(CGRect)frame widthPerSecond:(CGFloat)width themeColor:(UIColor *)color NS_DESIGNATED_INITIALIZER;
+
+@end
+
+NS_ASSUME_NONNULL_END
