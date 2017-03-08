026f8b0de842534e7629fa014d3e1c54738ea1d2
Naoto Onishi <onikiri@Naotos-MacBook-Pro.local>
2017-03-08 11:55:36 +1100

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+mkdir tools
+mkdir tools/nuget
 curl -o ./tools/nuget/nuget.exe -k https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 mono tools/nuget/nuget.exe update -self
 mono tools/nuget/nuget.exe install Cake -OutputDirectory tools -ExcludeVersion
